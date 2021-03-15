3578c13d6a2d0aeb6e0dbc9c9d23d798bb6224c9
Holy7Hao <cr7mj@me.com>
2021-03-15 20:10:45 +0800

    not finished

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -7,7 +7,13 @@
 // @lc code=start
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+       HashMap<Integer, Integer> tracker = new HashMap<Integer, Integer>()
+       int len = num.length;
+       for(int i = 0; i < len; i++){
+           if(tracker.containsKey(nums[i])){
+
+           }
+       }  
     }
 }
 // @lc code=end
